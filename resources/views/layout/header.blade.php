e51559be0c49c1d2baa0979259a5c3107543cd75
sisil <sisil@gmail.com>
2025-04-14 09:08:33 +0700

    nambah style dikit

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -5,7 +5,7 @@
 
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
-        <img src="{{asset('img/logo.png')}}" class="rounded-circle">
+        <img src="{{asset('img/logo.png')}}" class="rounded-circle" style="max-height: 55px;">
         <span class="d-none d-lg-block">SINAR</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
