123c3910c052419b84802fdf9096561c0b07b81d
naufal <miosan389@gmail.com>
2025-12-10 09:54:16 +0800

    feat(ui): restructure sidebar navigation with collapsible sections for better organization
    - Group related report types into collapsible dropdowns (Laporan Pasien, Laporan Rujukan, Laporan RL)
    - Add hierarchical navigation with proper icons and indentation
    - Maintain all existing routes and functionality
    - Improve visual organization and user experience

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -77,64 +77,77 @@ $peg = DB::table('pegawai')
 
       <li class="nav-heading">LAPORAN</li>
 
+      <!-- Laporan Rekam Medis -->
       <li class="nav-item">
         <a class="nav-link collapsed" href="{{ route('kelengkapan') }}">
           <i class="ri-bar-chart-grouped-fill"></i>
           <span>Laporan Rekam Medis</span>
         </a>
       </li>
-      
+
+      <!-- Laporan Pasien -->
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('laporan.pasien-meninggal') }}">
-          <i class="ri-bar-chart-grouped-fill"></i>
-          <span>Data pasien Meninggal</span>
+        <a class="nav-link collapsed" data-bs-target="#laporan-pasien-nav" data-bs-toggle="collapse" href="#">
+          <i class="ri-user-heart-fill"></i><span>Laporan Pasien</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
+        <ul id="laporan-pasien-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="{{ route('laporan.pasien-meninggal') }}">
+              <i class="bi bi-circle"></i><span>Data Pasien Meninggal</span>
+            </a>
+          </li>
+          <li>
+            <a href="{{ route('laporan.laporan_persalinan') }}">
+              <i class="bi bi-circle"></i><span>Laporan Persalinan</span>
+            </a>
+          </li>
+        </ul>
       </li>
 
+      <!-- Laporan Rujukan -->
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('laporan.rujukan-keluar') }}">
-          <i class="ri-bar-chart-grouped-fill"></i>
-          <span>Laporan Rujukan Keluar</span>
+        <a class="nav-link collapsed" data-bs-target="#laporan-rujukan-nav" data-bs-toggle="collapse" href="#">
+          <i class="ri-hospital-fill"></i><span>Laporan Rujukan</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
+        <ul id="laporan-rujukan-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="{{ route('laporan.rujukan-keluar') }}">
+              <i class="bi bi-circle"></i><span>Rujukan Keluar</span>
+            </a>
+          </li>
+          <li>
+            <a href="{{ route('laporan.rujukan-masuk') }}">
+              <i class="bi bi-circle"></i><span>Rujukan Masuk</span>
+            </a>
+          </li>
+        </ul>
       </li>
 
+      <!-- Laporan RL (SIRS) -->
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('laporan.rujukan-masuk') }}">
-          <i class="ri-bar-chart-grouped-fill"></i>
-          <span>Laporan Rujukan Masuk</span>
+        <a class="nav-link collapsed" data-bs-target="#laporan-rl-nav" data-bs-toggle="collapse" href="#">
+          <i class="ri-file-list-3-fill"></i><span>Rekapitulasi Laporan</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
+        <ul id="laporan-rl-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="{{ route('laporan.rujukan-rekap') }}">
+              <i class="bi bi-circle"></i><span>RL 3.10 Rekapitulasi Rujukan</span>
+            </a>
+          </li>
+          <li>
+            <a href="{{ route('morbiditas-rawat-inap') }}">
+              <i class="bi bi-circle"></i><span>RL 4.1 Morbiditas Rawat Inap</span>
+            </a>
+          </li>
+          <li>
+            <a href="{{ route('morbiditas-rawat-jalan') }}">
+              <i class="bi bi-circle"></i><span>RL 5.1 Morbiditas Rawat Jalan</span>
+            </a>
+          </li>
+        </ul>
       </li>
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('laporan.laporan_persalinan') }}">
-          <i class="ri-bar-chart-grouped-fill"></i>
-          <span>Laporan Persalinan</span>
-        </a>
-      </li>
-
-      <li class="nav-item">
-          <a class="nav-link collapsed" href="{{ route('laporan.rujukan-rekap') }}">
-              <i class="ri-bar-chart-grouped-fill"></i>
-              <span>Rekapitulasi Rujukan</span>
-          </a>
-      </li>
-
-      <li class="nav-item">
-          <a class="nav-link collapsed" href="{{ route('morbiditas-rawat-jalan') }}">
-              <i class="ri-bar-chart-grouped-fill"></i>
-              <span>Morbiditas Pasien Rawat Jalan</span>
-          </a>
-      </li>
-
-      <li class="nav-item">
-          <a class="nav-link collapsed" href="{{ route('morbiditas-rawat-inap') }}">
-              <i class="ri-bar-chart-grouped-fill"></i>
-              <span>Morbiditas Pasien Rawat Inap</span>
-          </a>
-      </li>
-      <!-- End Laporan Page Nav -->
-      
-
+      <!-- Laporan Farmasi -->
       <li class="nav-item">
         <a class="nav-link collapsed" href="{{ route('totalresep') }}">
           <i class="bx bxs-capsule"></i>
@@ -150,7 +163,6 @@ $peg = DB::table('pegawai')
           <span>Sign Out</span>
         </a>
       </li>
-<!-- End Laporan Page Nav -->
 
     </ul>
 
